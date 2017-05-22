dda43321515490af09b49f855dd00e63ce2d164c
Sysliklol <vkharko@gmail.com>
2017-05-22 20:12:06 +0300

    Home_work2

---
+++ Assets/scripts/LevelController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+    
+    public static LevelController current;
+    
+    void Awake()
+    {
+        current = this;
+    }
+
+
+    Vector3 startingPosition;
+    public void setStartPosition(Vector3 pos)
+    {
+        this.startingPosition = pos;
+    }
+    public void onRabitDeath(HeroRabbit rabit)
+    {
+        
+        rabit.transform.position = this.startingPosition;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
